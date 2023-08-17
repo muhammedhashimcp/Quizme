41f267993cb0c3ddad93049b5dfe212b5c4b8654
Muhammed Hashim CP <cpmohdhashim@gmail.com>
2023-08-17 20:12:02 +0530

    resolved provider component import in ./src/app/layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import Providers from '@/components/Providers'
+import Providers from '@/components/providers'
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
